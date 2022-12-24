b6f7e0d79f157ed304305692065bab2ec5fed7dd
et-codes <104942211+et-codes@users.noreply.github.com>
2022-12-24 10:42:24 -0700

    Create GameBoard class, DisplayBoard() method.

---
+++ BattleshipApp/Battleship/GameBoard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    public class GameBoard
+    {
+        private static int size = 10;
+        public char[,] board = new char[size, size];
+
+        public GameBoard()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    board[i, j] = '-';
+                }
+            }
+        }
+
+        public void DisplayBoard()
+        {
+            for (int i = size - 1; i >= 0; i--)
+            {
+                Console.Write((i+1).ToString().PadRight(2));
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write($" {board[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("0  1  2  3  4  5  6  7  8  9  10");
+        }
+    }
+}
--- BattleshipApp/Battleship/Program.cs
+++ BattleshipApp/Battleship/Program.cs
@@ -1,3 +1,19 @@
-﻿
+﻿/*
+Rules
+    Project written in C# (.NET Core)
+    Game grid will be 10x10
+    User will be allowed 8 guesses
+    When the battleship is hit 5 times it is considered sunk
+Deliverables
+    Computer will randomly assign it’s battleship within the dimensions of the grid
+    The user will be prompted to select a grid to fire at.
+    After each hit/miss, the user will be informed of all prior results
+    If a user inputs an incorrect value, user will be notified of the error and re-prompted
+*/
+
+using Battleship;
+
+GameBoard board= new GameBoard();
+board.DisplayBoard();
 
 
